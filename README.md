4924f8eabd27d4fe6997c2b763c8681364417bd1
Muhammad Faisal Amir <faisalamircs@gmail.com>
2023-04-06 13:40:28 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Music Compressor
+- v1.0.0 Development
+
+## Reference
+- w3school.com - https://www.w3schools.com/python
+
+## Tools
+- Python (SDK) - https://www.python.org/downloads/
+- PyCharm JetBrain Community (IDE) - https://www.jetbrains.com/pycharm/download/download-thanks.html?platform=windows&code=PCC
+- Git (Windows) - https://git-scm.com/download/win 
+
+## Colaborator
+Very open to anyone, I'll write your name under this, please contribute by sending an email to me
+
+- Mail To faisalamircs@gmail.com
+- Subject : Github _ [Github-Username-Account] _ [Language] _ [Repository-Name]
+- Example : Github_amirisback_kotlin_admob-helper-implementation
+
+Name Of Contribute
+- Muhammad Faisal Amir
+- Waiting List
+- Waiting List
+
+Waiting for your contribute
+
+## Attention !!!
+- Please enjoy and don't forget fork and give a star
+- Don't Forget Follow My Github Account
---
+++ main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+from os.path import basename
+
+from pydub import AudioSegment
+
+# folder path
+dir_path = input("Enter dir path: ")
+
+# list file and directories
+list_files = os.listdir(dir_path)
+
+# Directory
+dir_compressed = "compressed"
+# Path
+path = os.path.join(dir_path, dir_compressed)
+
+try:
+    os.makedirs(path, exist_ok=True)
+    print("Directory '%s' created successfully" % dir_compressed)
+except OSError as error:
+    print("Directory '%s' can not be created" % dir_compressed)
+
+
+def compress_mp3_file(mp3_file_path):
+    if not mp3_file_path.endswith(".mp3"): return
+
+    output_file_path = dir_path + "/" + dir_compressed + "/" + "{}.mp3".format(
+        os.path.splitext(basename(mp3_file_path))[0])
+    print("\nProcessing {} ==> {}".format(mp3_file_path, output_file_path))
+
+    audio_file = AudioSegment.from_file(mp3_file_path, "mp3")
+    frame_rate = audio_file.frame_rate
+    bytes_per_sample = audio_file.sample_width
+
+    if frame_rate == 11025:
+        print("frame rate is already 11025, ignore.")
+        return
+
+    print("frame_rate {} ==> {}".format(frame_rate, "11025"))
+    audio_file.export(output_file_path, format="mp3", parameters=["-ar", "11025"])
+
+
+for list_file in list_files:
+    data = dir_path + "/" + list_file
+    compress_mp3_file(data)
+
+print("\nDONE......................")
